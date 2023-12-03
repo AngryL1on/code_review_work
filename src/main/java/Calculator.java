4dac1ffe0f470059a3cb7a22fb6c31d185eed760
Vadim <vadim.lushina@gmail.com>
2023-12-03 22:29:45 +0300

    Update README and bring the code into codestyle

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,20 +1,24 @@
 public class Calculator {
-    public int add(int a, int b){
-        return a+b;
+    public int add(int a, int b) {
+        return a + b;
     }
-    public int dif(int a, int b){
-       return a-b;
+
+    public int dif(int a, int b) {
+        return a - b;
     }
-    public int div(int a, int b)throws Exception{
-        if(a==0 || b==0) {
+
+    public int div(int a, int b) throws Exception {
+        if (a == 0 || b == 0) {
             throw new Exception("Operation cannot be perfomed");
         }
-        return a/b;
+        return a / b;
     }
-    public int times(int a, int b){
-        return a*b;
+
+    public int times(int a, int b) {
+        return a * b;
     }
-    public int solver(int a,  int b, int c) throws Exception{
-        return times(a,a)+times(b,b)+add(a,b)+div(a,c)+dif(c,b);
+
+    public int solver(int a, int b, int c) throws Exception {
+        return times(a, a) + times(b, b) + add(a, b) + div(a, c) + dif(c, b);
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -3,31 +3,31 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
-
     Calculator calculator = new Calculator();
+
     @Test
     void add() {
-        assertEquals(2, calculator.add(1,1));
+        assertEquals(2, calculator.add(1, 1));
     }
 
     @Test
     void dif() {
-        assertEquals(1, calculator.dif(2,1));
+        assertEquals(1, calculator.dif(2, 1));
     }
 
     @Test
-    void div() throws Exception{
-        assertEquals(3, calculator.div(6,2));
-        assertThrows(Exception.class, ()->calculator.div(2,0));
+    void div() throws Exception {
+        assertEquals(3, calculator.div(6, 2));
+        assertThrows(Exception.class, () -> calculator.div(2, 0));
     }
 
     @Test
     void times() {
-        assertEquals(4, calculator.times(2,2));
+        assertEquals(4, calculator.times(2, 2));
     }
 
     @Test
-    void solver() throws Exception{
-        assertEquals(121, calculator.solver(10,2,5));
+    void solver() throws Exception {
+        assertEquals(121, calculator.solver(10, 2, 5));
     }
 }
