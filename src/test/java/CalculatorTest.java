1f5605968f221a085779e547918af7fc5598ca49
AngryL1on <95445205+AngryL1on@users.noreply.github.com>
2023-12-03 21:45:22 +0300

    Update README, implement all methods of the Calculator and CalculatorTest classes

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -3,29 +3,42 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    Calculator calculator = new Calculator();
 
     @Test
     void add() {
-        //TODO inser your realisation in test-method add
+        assertEquals(5, calculator.add(2, 3));
+        assertEquals(-7, calculator.add(-4, -3));
     }
 
     @Test
     void dif() {
-        //TODO inser your realisation in test-method dif
+        assertEquals(3, calculator.dif(6, 3));
+        assertEquals(9, calculator.dif(6, -3));
     }
 
     @Test
-    void div() {
-        //TODO inser your realisation in test-method div
+    void div() throws Exception {
+        assertEquals(5, calculator.div(45, 9));
+        assertThrows(Exception.class, () -> calculator.div(4, 0));
     }
 
     @Test
     void times() {
-        //TODO inser your realisation in test-method times
+        assertEquals(6, calculator.times(2, 3));
+        assertEquals(-12, calculator.times(4, -3));
+        assertEquals(0, calculator.times(4, 0));
     }
 
     @Test
-    void solver() {
-        //TODO inser your realisation in test-method solver
+    void solver() throws Exception {
+        // Both parameters come non-zero
+        assertEquals(-2, calculator.solver(4, 8));
+        assertEquals(2, calculator.solver(4, -8));
+
+        // One or both of the parameters is zero
+        assertThrows(Exception.class, () -> calculator.solver(0, -5));
+        assertThrows(Exception.class, () -> calculator.solver(5, 0));
+        assertThrows(Exception.class, () -> calculator.solver(0, 0));
     }
 }
